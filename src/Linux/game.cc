781325ce5afaa05bcb5dd091b02775f1c6a9f2bb
BitTim <tim.anhalt.2004@protonmail.com>
2020-03-05 13:10:24 +0100

    Updated to utilize the Logger

--- src/Linux/game.cc
+++ src/Linux/game.cc
@@ -1,4 +1,5 @@
 #include "../lib/game.hh"
+#include "../lib/logger.hh"
 
 SDL_Renderer* Game::renderer = nullptr;
 SDL_Event Game::event;
@@ -6,16 +7,29 @@ bool Game::running = true;
 
 void Game::init(std::string title, int x, int y, int w, int h, bool fullscreen)
 {
+  Logger::init();
+  Logger::log("init", "LogFile initialized");
+
   SDL_Init(SDL_INIT_EVERYTHING);
+  Logger::log("init", "Initialized SDL");
+
   window = SDL_CreateWindow(title.c_str(), x, y, w, h, fullscreen ? SDL_WINDOW_FULLSCREEN : 0);
+  Logger::log("init", "Created window");
   renderer = SDL_CreateRenderer(window, -1, 0);
+  Logger::log("init", "Created renderer");
+
   SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+  Logger::log("init", "Set Blendmode to BLEND");
 }
 
 void Game::handleEvents()
 {
   SDL_WaitEvent(&event);
-  if(event.type == SDL_QUIT) running = false;
+  if(event.type == SDL_QUIT)
+  {
+    Logger::log("event", "Received SDL_QUIT event");
+    running = false;
+  }
 }
 
 void Game::update() { }
@@ -33,8 +47,14 @@ void Game::render()
 void Game::terminate()
 {
   SDL_DestroyRenderer(renderer);
+  Logger::log("terminate", "Destroyed renderer");
   SDL_DestroyWindow(window);
+  Logger::log("terminate", "Destroyed window");
+
   SDL_Quit();
+  Logger::log("terminate", "Quitted SDL");
+
+  Logger::terminate();
 }
 
 bool Game::isRunning() { return running; }
