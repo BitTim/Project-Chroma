4b1ba221dd42b124b13e591b604f3e1c1f682176
BitTim <tim.anhalt.2004@protonmail.com>
2020-03-05 12:13:46 +0100

    Added Frame Limiting

--- src/Linux/main.cc
+++ src/Linux/main.cc
@@ -2,6 +2,11 @@
 
 Game* game;
 
+int targetFPS = 60;
+int targetFrameDelay = 1000 / targetFPS;
+Uint32 frameStart;
+int frameTime;
+
 int main()
 {
   game = new Game();
@@ -9,9 +14,14 @@ int main()
 
   while(game->isRunning())
   {
+    frameStart = SDL_GetTicks();
+
     game->handleEvents();
     game->update();
     game->render();
+
+    frameTime = SDL_GetTicks() - frameStart;
+    if(targetFrameDelay > frameTime) SDL_Delay(targetFrameDelay - frameTime);
   }
 
   game->terminate();
